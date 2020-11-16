7e60499bf1c0d35889c194513545a225bfdf7638
Yaroslava <72552637+Yyaroslava@users.noreply.github.com>
2020-11-16 13:13:41 +0200

    Add files via upload

---
+++ lab6.3iter.cpp
@@ -0,0 +1,41 @@
+// Ітераційний спосіб
+
+#include <iostream> 
+#include <iomanip> 
+#include <time.h> 
+
+using namespace std;
+
+void Create(int* c, const int size, const int Low, const int High) {
+    for (int i = 0; i < size; i++)
+        c[i] = Low + rand() % (High - Low + 1);
+}
+
+void Print(int* c, const int size) {
+    for (int i = 0; i < size; i++)
+        cout << setw(4) << c[i];
+    cout << endl;
+}
+
+template <typename T> // шаблон функції
+T Min(T* c, const int size) {
+    T min = c[0];
+    for (int i = 1; i < size; i++)
+        if (c[i] < min) min = c[i];
+    return min;
+}
+
+int main() {
+    srand((unsigned)time(NULL));  // ініціалізація генератора випадкових чисел  
+    const int n = 12;
+    int c[n];
+
+    int Low = 15;
+    int High = 85;
+
+    Create(c, n, Low, High);
+    Print(c, n);
+    cout << "Minimum = " << Min<int>(c, n) << endl;
+
+    return 0;
+}
